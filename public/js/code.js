38b9e749139388fbd8d9393428dc14ecc53754b9
changhan <changhanlee@gmail.com>
2015-10-31 09:19:06 -0400

    better tab

--- public/js/code.js
+++
@@ -1,6 +0,0 @@
-document.getElementById("mytextarea").onkeydown = function(e) {
-  if (e.keyCode == 9) {
-    e.preventDefault();
-    document.getElementById("mytextarea").value += '\t';
-  }
-};
---
+++ public/js/edit-space.js
@@ -0,0 +1,12 @@
+document.getElementById("edit-space").onkeydown = function(e) {
+  if (e.keyCode == 9) {
+    e.preventDefault();
+    var mark = this.selectionStart;
+    this.value =
+      this.value.slice(0, this.selectionStart) +
+      '\t' +
+      this.value.slice(this.selectionStart);
+    this.selectionStart = mark + 1;
+    this.selectionEnd = mark + 1;
+  }
+};
