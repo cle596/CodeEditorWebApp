75c8e8a4779712b6a5796d5a5a73232caf65fbab
changhan <changhanlee@gmail.com>
2015-11-02 00:16:34 +0000

    doing some s3 stuff now

--- app.js
+++ app.js
@@ -1,10 +1,28 @@
-var express = require('express');
+var AWS = require('aws-sdk');
+var s3 = new AWS.S3();
+var fs = require('fs');
 
+var express = require('express');
 var app = express();
 
 app.use(express.static('public'));
-
-app.get('/',function (req,res){
+app.get('/', function(req, res) {});
+app.post('/push', function(req, res) {
+  req.on("data", function(data) {
+    console.log(data.toString());
+    var params = {
+      Bucket: 'code-editor',
+      Key: 'shit',
+      Body: data.toString()
+    };
+    s3.putObject(params, function(err, data) {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log("Successfully uploaded: " + params.Key + " " + params.Body);
+      }
+    });
+  });
+  res.send("shitreply");
 });
-
-app.listen(8000,'0.0.0.0');
+app.listen(8000, '0.0.0.0');
---
+++ public/js/xhr.js
@@ -0,0 +1,13 @@
+var xhttp = new XMLHttpRequest();
+
+xhttp.onreadystatechange = function() {
+  if (xhttp.readyState == 4 && xhttp.status == 200) {
+   document.getElementById("demo").innerHTML = xhttp.responseText;
+  }
+}
+
+document.getElementById("push").onclick = function(e){
+  xhttp.open("POST","push",true);
+  xhttp.setRequestHeader("Content-type","text/plain");
+  xhttp.send("hello");
+}
