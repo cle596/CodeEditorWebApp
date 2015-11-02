b9e1e710d442954ea0528e4e8db96fe7433c2a2c
changhan <changhanlee@gmail.com>
2015-11-02 11:21:47 +0000

    cookie middleware listing order matters

--- app.js
+++ app.js
@@ -1,43 +1,41 @@
 var AWS = require('aws-sdk');
 var s3 = new AWS.S3();
 var fs = require('fs');
-
 var crypto = require('crypto');
-
-var redis = require("redis"),
-    client = redis.createClient();
-
+var express = require('express');
 var session = require('express-session');
 var RedisStore = require('connect-redis')(session);
-
-var express = require('express');
+var cookieParser = require('cookie-parser');
 var app = express();
-
-app.use(express.static('public'));
-
+//app.use(cookieParser());
 app.use(session({
+  name: "mycookie",
   genid: function(req) {
     var sha = crypto.createHash('sha256');
     sha.update(Math.random().toString());
     return sha.digest('hex');
   },
+  cookie: {
+    secure: false,
+    httpOnly: false,
+    maxAge: 60000
+  },
+  /*
+  store: new MemoryStore,
   store: new RedisStore({
     host: 'localhost'
   }),
-  secret: 'sillysecretkey',
-  resave: false,
-  saveUninitialized: false
+  */
+  secret: 'ejawofjewaoigjaweoigjwega',
+  resave: true,
+  rolling: true,
+  saveUninitialized: true,
+  unset: 'keep'
 }));
-
-app.use(function (req, res, next) {
-  if (!req.session) {
-    return next(new Error('oh no')) // handle error
-  }
-  next() // otherwise continue
-})
-
-app.get('/', function(req, res) {
-  console.log(req.session);
+app.use(express.static('public'));
+app.use(function(err, req, res, next) {
+  console.log(req.session.id);
+  next();
 });
 app.post('/push', function(req, res) {
   req.on("data", function(data) {
@@ -47,6 +45,7 @@ app.post('/push', function(req, res) {
       Key: 'shit',
       Body: data.toString()
     };
+    console.log(params.Body);
     s3.putObject(params, function(err, data) {
       if (err) {
         console.log(err);
