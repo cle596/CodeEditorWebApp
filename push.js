13527b0f2ac3d8f52d5cec22e8d2e739942e716f
changhan <changhanlee@gmail.com>
2015-11-03 11:50:11 -0500

    damn route

--- push.js
+++ push.js
@@ -1,24 +1,27 @@
-function router(req, res) {
-  var push_data = "";
-  req.on("data", function(data) {
-    push_data += data;
-  });
-  req.on("end", function() {
-    var params = {
-      Bucket: 'code-editor/brosf',
-      Key: 'textdoc',
-      Body: push_data.toString()
-    };
-    s3.putObject(params, function(err, data) {
-      if (err) {
-        console.log(err);
-      } else {
-        console.log("Uploaded to: " + params.Key);
-        push_data = "";
-      }
+module.exports = (function() {
+  'use strict';
+  var router = require('express').Router();
+  router.get('/', function(req, res) {
+    var push_data = "";
+    req.on("data", function(data) {
+      push_data += data;
     });
+    req.on("end", function() {
+      var params = {
+        Bucket: 'code-editor/brosf',
+        Key: 'textdoc',
+        Body: push_data.toString()
+      };
+      s3.putObject(params, function(err, data) {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log("Uploaded to: " + params.Key);
+          push_data = "";
+        }
+      });
+    });
+    res.send("Saved to cloud.");
   });
-  res.send("Saved to cloud.");
-}
-
-module.export = router;
+  return router;
+})();
