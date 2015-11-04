1d86c6792784d2f371cf4fa002951a700aa938cd
changhan <changhanlee@gmail.com>
2015-11-04 18:48:03 +0000

    came back to cb

--- login.js
+++ login.js
@@ -1,26 +1,25 @@
 var redis = require("redis");
 var client = redis.createClient();
-var prominence = require("prominence");
-//var express = require("express");
-var express = prominence(require('express'));
+//var prominence = require("prominence");
+var express = require("express");
+//var express = prominence(require('express'));
 
 module.exports = (function() {
-  'use strict';
-  var router = express.Router();
-  var logged_in = false;
-  var response = "";
-  router.post('/login')
-    .then(function(req, res) {
-      req.on("data", function(data) {
-        data = JSON.parse(data.toString('utf8'));
-        client.get(data.user, function(err, reply) {
-          if (data.pw == reply) {
-            res.send("Logged in!");
-          } else {
-            res.send("Login fail!");
-          }
-        });
-      });
+    'use strict';
+    var router = express.Router();
+    var logged_in = false;
+    var response = "";
+    router.post('/login',function(req, res) {
+	req.on("data", function(data) {
+            data = JSON.parse(data.toString('utf8'));
+            client.get(data.user, function(err, reply) {
+		if (data.pw == reply) {
+		    res.send("Logged in!");
+		} else {
+		    res.send("Login fail!");
+		}
+            });
+	});
     });
-  return router;
+    return router;
 })();
