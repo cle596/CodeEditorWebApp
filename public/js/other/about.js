27b368a2e52e4a85e7ce4ac0dd82894ed39ecd6b
changhan <changhanlee@gmail.com>
2015-11-04 22:39:54 +0000

    johda

--- public/js/other/about.js
+++ public/js/other/about.js
@@ -1,5 +1,6 @@
 var about = "Code Editor is an online IDE for editing documents in a cloud storage and provides version control utilities to give programmers a work environment in the browser.";
 
-document.getElementById("about").onclick = function(e){
-  document.getElementById("edit-space").value = about;
-}
+document.getElementById("about").addEventListener("click",function(e){
+  edit_space.value = about;
+});
+
--- public/js/other/tabs.js
+++ public/js/other/tabs.js
@@ -1,1 +1,27 @@
-document.getElementById("tab-1").innerText = "tub";
+var tab1 = document.getElementById("tab-1");
+var tab2 = document.getElementById("tab-2");
+var tab3 = document.getElementById("tab-3");
+var tab4 = document.getElementById("tab-4");
+
+var tab1_content = "tab1";
+var tab2_content = "tab2";
+var tab3_content = "tab3";
+var tab4_content = "tab4";
+
+var edit_space = document.getElementById("edit-space");
+tab1.addEventListener("click",function(e){
+    edit_space.value = tab1_content;
+});
+
+tab2.addEventListener("click",function(e){
+    edit_space.value = tab2_content;
+});
+
+tab3.addEventListener("click",function(e){
+    edit_space.value = tab3_content;
+});
+
+tab4.addEventListener("click",function(e){
+    edit_space.value = tab4_content;
+});
+
