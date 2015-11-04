cfd1f5d263907e29143dc89a3b738b0c8ebaa386
changhan <changhanlee@gmail.com>
2015-11-04 13:41:01 -0500

    prominence

--- login.js
+++ login.js
@@ -1,25 +1,26 @@
-var express = require('express');
-var redis = require("redis"),
-client = redis.createClient();
-var StringDecoder = require('string_decoder').StringDecoder;
+var redis = require("redis");
+var client = redis.createClient();
+var prominence = require("prominence");
+var express = require("express");
+//var express = prominence(require('express'));
 
 module.exports = (function() {
-    'use strict';
-    var router = express.Router();
-    var logged_in = false;
-    var response = "";
-    var decoder = new StringDecoder('utf8');
-    router.post('/login', function(req, res) {
-	req.on("data", function(data) {
-	    data = JSON.parse(data.toString('utf8'));
-	    client.get(data.user, function(err, reply) {
-		if (data.pw == reply) {
-		    res.send("Logged in!");
-		} else {
-		    res.send("Login fail!");
-		}
-	    }); 
-	});
-    });
-    return router;
+  'use strict';
+  var router = prominence(express.Router());
+  var logged_in = false;
+  var response = "";
+  router.post('/login')
+    .then(function(req, res) {
+      req.on("data", function(data) {
+        data = JSON.parse(data.toString('utf8'));
+        client.get(data.user, function(err, reply) {
+          if (data.pw == reply) {
+            res.send("Logged in!");
+          } else {
+            res.send("Login fail!");
+          }
+        });
+      });
+    })
+  return router;
 })();
