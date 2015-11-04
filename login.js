e48e37b54811a13a1a55ff4f2462132233944052
changhan <changhanlee@gmail.com>
2015-11-04 19:00:34 +0000

    cant use bluebird

--- login.js
+++ login.js
@@ -1,12 +1,16 @@
 var redis = require("redis");
 var client = redis.createClient();
 //var prominence = require("prominence");
+var Promise = require("bluebird");
 var express = require("express");
 //var express = prominence(require('express'));
+//var pexpress = Promise.promisifyAll(require("express"));
 
 module.exports = (function() {
     'use strict';
+  //  console.log(typeof pexpress);
     var router = express.Router();
+//    var post = Promise.promisify(require("express").Router().post);
     var logged_in = false;
     var response = "";
     router.post('/login',function(req, res) {
